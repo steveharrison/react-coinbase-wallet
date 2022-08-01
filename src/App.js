c7136a4fe97e69196fa5c3cf6e5d6d0db8440b49
Steve Harrison <steveharrison.dev@icloud.com>
2022-08-01 15:37:04 +1000

    Added Coinbase Wallet integration.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { WalletLinkConnector } from "@web3-react/walletlink-connector";
+import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
+import { InjectedConnector } from "@web3-react/injected-connector";
+import { useWeb3React } from '@web3-react/core';
+
+const CoinbaseWallet = new WalletLinkConnector({
+  url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+  appName: "Web3-react Demo",
+  supportedChainIds: [1, 3, 4, 5, 42],
+});
+
+const WalletConnect = new WalletConnectConnector({
+ rpcUrl: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+ bridge: "https://bridge.walletconnect.org",
+ qrcode: true,
+});
+
+const Injected = new InjectedConnector({
+ supportedChainIds: [1, 3, 4, 5, 42]
+});
+
 function App() {
+  const {
+    activate,
+    deactivate,
+    active,
+    chainId,
+    account
+  } = useWeb3React();
+  console.log('active,chainId,account', active, chainId, account);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Connection Status: {active}</div>
+      <div>Account: {account}</div>
+      <div>Network ID: {chainId}</div>
+      <button onClick={() => { activate(CoinbaseWallet) }}>Coinbase Wallet</button>
+      <button onClick={() => { activate(WalletConnect) }}>Wallet Connect</button>
+      <button onClick={() => { activate(Injected) }}>Metamask</button>
+      <button onClick={deactivate}>Disconnect</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Web3ReactProvider } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
+
+function getLibrary(provider) {
+  return new Web3Provider(provider);
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <App />
+    </Web3ReactProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
